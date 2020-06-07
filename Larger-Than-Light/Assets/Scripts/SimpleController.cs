f794ebb7299456a53f1e63a4c64c11b23da64809
mingxual <mingxual@usc.edu>
2020-06-07 04:09:57 -0800

    initial commit with Richard's project

---
+++ Larger-Than-Light/Assets/Scripts/SimpleController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    Rigidbody2D rb;
+    bool canJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && canJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+}
